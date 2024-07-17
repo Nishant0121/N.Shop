92240290a81ff3ec768243728dbca9083047d06f
Nishant Patil <142099904+Nishant0121@users.noreply.github.com>
2024-07-17 22:15:21 +0530

    1st

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173", // Replace with your frontend's actual origin
+    origin: "https://n-shop-lyart.vercel.app/", // Replace with your frontend's actual origin
   })
 );
 
